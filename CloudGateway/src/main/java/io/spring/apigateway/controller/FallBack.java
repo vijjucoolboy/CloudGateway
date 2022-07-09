31f36de5b3e470cd3c918f8a627f3b63acdb57c8
vijju <vijju@localhost>
2022-07-09 22:34:58 +0530

    First Commit for spring cloud gateway

---
+++ CloudGateway/src/main/java/io/spring/apigateway/controller/FallBack.java
@@ -0,0 +1,23 @@
+package io.spring.apigateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallBack {
+	
+	@RequestMapping("/orderFallback")
+	public Mono<String> orderServiceFallback(){
+		
+		return Mono.just("Order service is taking too long to response. Please try again later!");
+	}
+	
+	@RequestMapping("/paymentFallback")
+	public Mono<String> paymentServiceFallback(){
+		
+		return Mono.just("Payment service is taking too long to response. Please try again later!");
+	}
+
+}
---
+++ CloudGateway/src/test/java/io/spring/apigateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
